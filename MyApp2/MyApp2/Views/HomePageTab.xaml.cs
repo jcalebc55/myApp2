afdab9dddc9cbcfe9523fbd6e4eef373a579c7d4
Jabez Vejendla <jcalebc55@gmail.com>
2020-06-24 17:04:29 -0700

    Changes for the Links

--- MyApp2/MyApp2/Views/HomePageTab.xaml.cs
+++ MyApp2/MyApp2/Views/HomePageTab.xaml.cs
@@ -20,26 +20,38 @@ namespace MyApp2.Views
         ObservableCollection<EmployeeResources> ers = new ObservableCollection<EmployeeResources>();
         public ObservableCollection<EmployeeResources> EmpResource { get { return ers; } }
 
-        public IList<EmployeeResources> empResource { get; private set; }
-        public HomePageTab ()
+        public IList<EmployeeResources> empResourceReminders { get; private set; }
+        public IList<EmployeeResources> empResourceLinks { get; private set; }
+        public HomePageTab()
         {
 
             InitializeComponent();
 
             // ObservableCollection allows items to be added after ItemsSource
             // is set and the UI will react to changes
-            
-        empResource = new List<EmployeeResources>();
-        
-        empResource.Add(new EmployeeResources { Reminder = "You may have received a letter from BAS about Medicare part D coverage – this is a required notice and applies only to employees who are Medicare eligible or have a dependent who is Medicare eligible. If neither situation applies to you, please ignore the letter. If either yourself or your dependent are eligible for Medicare, please contact HR for further information." });
 
-            empResource.Add(new EmployeeResources { Reminder = "For business travelers, beginning October 1, 2020, every traveler must present a REAL ID-compliant driver’s license, or another acceptable form of identification, to fly within the United States. Please check https://www.dhs.gov/real-id for more information." });
-            
-            EmployeeLinksView.ItemsSource = empResource; 
+            empResourceReminders = new List<EmployeeResources>();
+            empResourceLinks = new List<EmployeeResources>();
+
+            empResourceReminders.Add(new EmployeeResources { Reminder = "You may have received a letter from BAS about Medicare part D coverage – this is a required notice and applies only to employees who are Medicare eligible or have a dependent who is Medicare eligible. If neither situation applies to you, please ignore the letter. If either yourself or your dependent are eligible for Medicare, please contact HR for further information." });
+
+            empResourceReminders.Add(new EmployeeResources { Reminder = "For business travelers, beginning October 1, 2020, every traveler must present a REAL ID-compliant driver’s license, or another acceptable form of identification, to fly within the United States. Please check https://www.dhs.gov/real-id for more information." });
+
+            empResourceLinks.Add(new EmployeeResources { Reminder = "4/16/2020 - NDTI COVID-19 Policy", Link= "https://esp21.ndti.net/files/NDTICoronavirusPolicy.pdf" });
+            empResourceLinks.Add(new EmployeeResources{ Reminder = "4/15/2020 Covid-19 Update - Message from the NDTI CEO",Link= "https://esp21.ndti.net/files/covid19NDTICEOUpdate.pdf" });
+
+            EmployeeRemindersView.ItemsSource = empResourceReminders;
+            EmployeeLinksView.ItemsSource = empResourceLinks;
 
         }
 
-        
+        void OnListViewItemTapped(object sender, ItemTappedEventArgs e)
+        {
+              EmployeeResources tappedItem = e.Item as EmployeeResources;
+            
+                Device.OpenUri(new Uri(tappedItem.Link));
+            
+        }
     }
 
     
